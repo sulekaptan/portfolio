5019da4a260db2c9abac5694ab6b7f718b9f184d
Şule <sule.kaptan@bil.omu.edu.tr>
2022-12-02 16:42:42 +0300

    added navbar

--- components/Nav.jsx
+++ components/Nav.jsx
@@ -1,9 +1,107 @@
-import React from 'react'
+import React, { useState } from 'react'
+import Image from 'next/image'
+import Link from 'next/link'
+import { AiOutlineMenu, AiOutlineClose, AiOutlineMail } from 'react-icons/ai'
+import { FaLinkedinIn, FaGithub, FaTwitter } from 'react-icons/fa'
+import { BsFillPersonLinesFill } from 'react-icons/bs'
 
 const Nav = () => {
+
+  const [nav, setNav] = useState(false)
+
+  const handleNav = () => {
+    setNav(!nav)
+  }
+
   return (
-    <div>
-      
+    <div className='fixed w-full h-20 shadow-xl z-[100]'>
+      <div className='flex justify-between items-center w-full h-full px-2 2xl:px-16'>
+        <Image
+          src="/../public/assets/logo.png"
+          alt='/'
+          width='50'
+          height='50' />
+        <div>
+          <ul className='hidden md:flex'>
+            <Link href='#'>
+              <li className='ml-10 text-sm uppercase hover:border-b'>Anasayfa</li>
+            </Link>
+            <Link href='/'>
+              <li className='ml-10 text-sm uppercase hover:border-b'>Hakkında</li>
+            </Link>
+            <Link href='/'>
+              <li className='ml-10 text-sm uppercase hover:border-b'>Beceriler</li>
+            </Link>
+            <Link href='/'>
+              <li className='ml-10 text-sm uppercase hover:border-b'>Projeler</li>
+            </Link>
+            <Link href='/'>
+              <li className='ml-10 text-sm uppercase hover:border-b'>İletişim</li>
+            </Link>
+          </ul>
+          <div onClick={handleNav} className='md:hidden'>
+            <AiOutlineMenu size={25} />
+          </div>
+        </div>
+      </div>
+
+
+      <div className={nav ? 'fixed left-0 top-0 w-full h-screen bg-black/70' : ''}>
+        <div className=
+            {nav 
+                ? 'fixed left-0 top-0 w-[75%] sm:w-[60%] md:w-[45%] h-screen bg-[#ecf0f3] p-10 ease-in duration-500' 
+                : 'fixed left-[-100%] top-0 duration-500'}>
+          <div className='flex w-full items-center justify-between'>
+            <Image
+              src="/../public/assets/logo.png"
+              alt='/'
+              width='50'
+              height='35' />
+            <div onClick={handleNav} className='rounded-full shadow-lg shadow-gray-400 p-3 cursor-pointer'>
+              <AiOutlineClose />
+            </div>
+          </div>
+          <div className='border-b border-gray-300 my-4'>
+            <p className='w-[85%] md:w-[90] py-4'>I will try my best</p>
+          </div>
+          <div>
+            <ul>
+              <Link href='/'>
+                <li className=' py-4 text-sm'>Anasayfa</li>
+              </Link>
+              <Link href='/'>
+                <li className='text-sm py-4'>Hakkında</li>
+              </Link>
+              <Link href='/'>
+                <li className='text-sm py-4'>Beceriler</li>
+              </Link>
+              <Link href='/'>
+                <li className='text-sm py-4'>Projeler</li>
+              </Link>
+              <Link href='/'>
+                <li className='text-sm py-4'>İletişim</li>
+              </Link>
+            </ul>
+            <div className='pt-40'>
+              <p className='uppercase tracking-widest text-[#5651e5]'>Let's Connect</p>
+              <div className='flex items-center justify-between my-4 w-full sm:w-[80%]'>
+                <div className='rounded-full shadow-lg shadow-gray-400 p-3 cursor-pointer hover:scale-105 ease-in duration-300'>
+                  <FaLinkedinIn />
+                </div>
+                <div className='rounded-full shadow-lg shadow-gray-400 p-3 cursor-pointer hover:scale-105 ease-in duration-300'>
+                  <FaGithub />
+                </div>
+                <div className='rounded-full shadow-lg shadow-gray-400 p-3 cursor-pointer hover:scale-105 ease-in duration-300'>
+                  <AiOutlineMail />
+                </div>
+                <div className='rounded-full shadow-lg shadow-gray-400 p-3 cursor-pointer hover:scale-105 ease-in duration-300'>
+                  <FaTwitter />
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
     </div>
   )
 }
